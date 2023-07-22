9cf47809f8b80a5794b240ba1fddcd9a6bfebe5f
Sandipanassasign <baralsandipan@gmail.com>
2023-07-22 23:46:10 +0530

    basetest extends updated for HomePageTest.java

--- src/test/java/sandipanacademy/Test/HomePageTest.java
+++ src/test/java/sandipanacademy/Test/HomePageTest.java
@@ -11,14 +11,13 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import sandipanacademy.TestComponents.BaseTest;
-import sandipanacademy.TestComponents.BaseTest_withSelGridSupport;
 import sandipanacademy.pageobjects.CartPage;
 import sandipanacademy.pageobjects.MyOrderPage;
 import sandipanacademy.pageobjects.PaymentsPage;
 import sandipanacademy.pageobjects.ProductCatalog;
 import sandipanacademy.pageobjects.ThankYouPage;
 
-public class HomePageTest extends BaseTest_withSelGridSupport{
+public class HomePageTest extends BaseTest{
 	
 	
 	
